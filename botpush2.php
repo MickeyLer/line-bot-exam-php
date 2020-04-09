45197fb77f94a1c506b8c8346a7385213a8685b8
MickeyLer <63302577+MickeyLer@users.noreply.github.com>
2020-04-09 21:40:44 +0700

    Update botpush2.php

--- botpush2.php
+++ botpush2.php
@@ -1,10 +1,4 @@
 <?php
-	mysql> CREATE USER 'u136290615_user'@'127.0.0.1' IDENTIFIED BY 'HYIegy`b';
-	mysql> GRANT ALL PRIVILEGES ON *.* TO 'u136290615_user'@'127.0.0.1'
-  	  ->     WITH GRANT OPTION;
-	mysql> CREATE USER 'u136290615_user'@'127.0.0.1' IDENTIFIED BY 'HYIegy`b';
-	mysql> GRANT ALL PRIVILEGES ON *.* TO 'u136290615_user'@'%'
-  	  ->     WITH GRANT OPTION;
 
 	$conn = new mysqli("127.0.0.1","u136290615_user","HYIegy`b","u136290615_sub");
 	// Check connection
