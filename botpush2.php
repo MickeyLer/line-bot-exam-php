358568d25f4c186d78bfbc65388b789a1ce8bf40
MickeyLer <63302577+MickeyLer@users.noreply.github.com>
2020-04-09 21:29:38 +0700

    Update botpush2.php

--- botpush2.php
+++ botpush2.php
@@ -1,7 +1,11 @@
 <?php
 
-	$db = new mysqli("localhost","u136290615_user1","HYIegy`b","u136290615_sub") or die ("connect error".mysqli_error());
-	
+	$conn = new mysqli("localhost","u136290615_user","HYIegy`b","u136290615_sub") or die ("connect error".mysqli_error());
+	// Check connection
+	if ($conn->connect_error) {
+   		die("Connection failed: " . $conn->connect_error);
+	}
+	echo "Connected successfully";
 ?>
 
  
