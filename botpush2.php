9e5cc2dab89b9f01a2ee0699f2a608622806c1d7
MickeyLer <63302577+MickeyLer@users.noreply.github.com>
2020-04-09 21:23:28 +0700

    Update botpush2.php

--- botpush2.php
+++ botpush2.php
@@ -2,9 +2,7 @@
 
 	$db = new mysqli("localhost","u136290615_user","HYIegy`b","u136290615_sub") or die ("connect error".mysqli_error());
 	$sql = $db->query("SELECT * FROM sub_autonotify");
-	while ($n = $sql->fetch_object()){
-		echo $n->name. "<br />";
-	}	
+	echo print _r ($sql);
 ?>
 
  
