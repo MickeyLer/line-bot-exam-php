58a772cf6166137a84da873e951e79b5bbc8fe9c
MickeyLer <63302577+MickeyLer@users.noreply.github.com>
2020-04-09 21:32:39 +0700

    Update botpush2.php

--- botpush2.php
+++ botpush2.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn = new mysqli("localhost","u136290615_user","HYIegy`b","u136290615_sub");
+	$conn = new mysqli("127.0.0.1","u136290615_user","HYIegy`b","u136290615_sub");
 	// Check connection
 	if ($conn->connect_error) {
    		die("Connection failed555: " . $conn->connect_error);
