d728f12f206f74fa07c50b7262b008060b8963a2
MickeyLer <63302577+MickeyLer@users.noreply.github.com>
2020-04-15 16:51:15 +0700

    Update test_linelog.php

--- test_linelog.php
+++ test_linelog.php
@@ -7,5 +7,20 @@ $json = file_get_contents('php://input');
 $request = json_decode($json, true);
 $queryText = $request["queryResult"]["queryText"];
 $userId = $request['originalDetectIntentRequest']['payload']['data']['source']['userId'];
-echo $json;
+
+require "vendor/autoload.php";
+
+$access_token = '9X89LSe2o0SVcq3UlQZpDgVxMa36OKjDi2GS99Svmthkml2Zf/nB8K5/yxzWq4VOrefvJZQsdHjeBjvLaGozxaU/YJqsKYE0TeeLV7oJgIzWeSEuJ/Ykv7HvXSjCJpTaeJVNuxx/JgZPcqHshvS+LgdB04t89/1O/w1cDnyilFU=';
+
+$channelSecret = '3295b0b1b17e3b0c4f1c1891db6fb289';
+
+$pushID = $userId;
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>
