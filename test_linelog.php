92cc1f54d74939bbbce03def87284639a8edd6b6
MickeyLer <63302577+MickeyLer@users.noreply.github.com>
2020-04-15 16:46:00 +0700

    Update test_linelog.php

--- test_linelog.php
+++ test_linelog.php
@@ -6,8 +6,5 @@ $json = file_get_contents('php://input');
 $request = json_decode($json, true);
 $queryText = $request["queryResult"]["queryText"];
 $userId = $request['originalDetectIntentRequest']['payload']['data']['source']['userId'];
-$myfile = fopen("log$date.txt", "a") or die("Unable to open file!");
-$log = $date."-".$time."\t".$userId."\t".$queryText."\n";
-fwrite($myfile,$log);
-fclose($myfile);
+echo $userId;
 ?>
