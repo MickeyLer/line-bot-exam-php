f555d77ef41bc9da7f54ac481dbaeb5fcfe588bf
MickeyLer <63302577+MickeyLer@users.noreply.github.com>
2020-04-15 20:59:15 +0700

    Update test_linelog2.php

--- test_linelog2.php
+++ test_linelog2.php
@@ -1,15 +1,26 @@
+<?php 
+	/*Get Data From POST Http Request*/
+	$datas = file_get_contents('php://input');
+	/*Decode Json From LINE Data Body*/
+	$deCode = json_decode($datas,true);
 
+	file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
 
+	$id = $deCode['events'][0]['source']['userId'];
+      $message = $arrayJson['events'][0]['message']['text'];
+	 
 
-
-
-
-$arrayPostData['to'] = $id;
+      $arrayPostData['to'] = $id;
       $arrayPostData['messages'][0]['type'] = "text";
       $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
       $arrayPostData['messages'][1]['type'] = "sticker";
       $arrayPostData['messages'][1]['packageId'] = "2";
       $arrayPostData['messages'][1]['stickerId'] = "34";
+
+      $arrayHeader = array();
+      $arrayHeader[] = "Content-Type: application/json";
+      $arrayHeader[] = "Authorization: Bearer ME3YfGVoiHzLsnp3pW1XSSFGmDpXoJPJw+b9dyB4c4kynw5eT+7DJeFzL+hgJ/V8VMAliTSYhy5BkI9HRac/69R/gzoz7iSWMVJEDVlFyNktzm+0+/XdB4ZKXPjLfZqwjJDgnHGbx2AXpAmKEU4zpgdB04t89/1O/w1cDnyilFU=";
+
       pushMsg($arrayHeader,$arrayPostData);
    }
    function pushMsg($arrayHeader,$arrayPostData){
