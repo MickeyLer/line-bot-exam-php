284b7dc6a22a9a835bcf981e4aad0225258d5e89
MickeyLer <63302577+MickeyLer@users.noreply.github.com>
2020-04-15 16:56:35 +0700

    Update test_linelog.php

--- test_linelog.php
+++ test_linelog.php
@@ -14,13 +14,13 @@ $access_token = '9X89LSe2o0SVcq3UlQZpDgVxMa36OKjDi2GS99Svmthkml2Zf/nB8K5/yxzWq4V
 
 $channelSecret = '3295b0b1b17e3b0c4f1c1891db6fb289';
 
-//$pushID = '$userId';
+$pushID = 'U5bf110dae6585f2fdf72f5f0a3fe9b09';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage($UserId, $textMessageBuilder);
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>
