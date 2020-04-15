0a01773d9beef474fc54c6b77438574b62bee1a3
MickeyLer <63302577+MickeyLer@users.noreply.github.com>
2020-04-15 19:43:17 +0700

    Update test_usergithub.php

--- test_usergithub.php
+++ test_usergithub.php
@@ -34,8 +34,8 @@ function getLINEProfile($datas)
    return $datasReturn;
 }
 
-//$userId = "U1a81dc36b7c95042c6ee4718cd5a7c18";
-//$LINEDatas['url'] = "https://api.line.me/v2/bot/profile/".$userId;
+$userId = "U5bf110dae6585f2fdf72f5f0a3fe9b09";
+$LINEDatas['url'] = "https://api.line.me/v2/bot/profile/".$userId;
 $LINEDatas['token'] = "ME3YfGVoiHzLsnp3pW1XSSFGmDpXoJPJw+b9dyB4c4kynw5eT+7DJeFzL+hgJ/V8VMAliTSYhy5BkI9HRac/69R/gzoz7iSWMVJEDVlFyNktzm+0+/XdB4ZKXPjLfZqwjJDgnHGbx2AXpAmKEU4zpgdB04t89/1O/w1cDnyilFU=";
 $results = getLINEProfile($LINEDatas);
 file_put_contents('log-profile.txt', $results['message'] . PHP_EOL, FILE_APPEND);
